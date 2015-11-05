920fe0a8c5b1d64f53a2a08dea2dfda2a7eb56a2
Alejano-Borhez <alexander.borohov17@gmail.com>
2015-11-06 02:54:36 +0300

    Template for service module&tests is done

---
+++ social-service/src/main/java/com/epam/brest/course2015/social/service/SocialServiceImpl.java
@@ -0,0 +1,50 @@
+package com.epam.brest.course2015.social.service;
+
+import com.epam.brest.course2015.social.core.User;
+
+import java.util.List;
+
+/**
+ * Created by alexander on 6.11.15.
+ */
+public class SocialServiceImpl implements SocialService {
+    @Override
+    public Integer addUser(User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Integer id) {
+
+    }
+
+    @Override
+    public void updateUser(Integer id) {
+
+    }
+
+    @Override
+    public User getUserById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public User getUserByLogin(String login) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public List<User> getFriends() {
+        return null;
+    }
+
+    @Override
+    public void addFriendship(Integer id1, Integer id2) {
+
+    }
+}
---
+++ social-service/src/test/java/com/epam/brest/course2015/social/service/SocialServiceImplTest.java
@@ -0,0 +1,39 @@
+package com.epam.brest.course2015.social.service;
+
+/**
+ * Created by alexander on 6.11.15.
+ */
+public class SocialServiceImplTest  {
+
+    public void testAddUser() throws Exception {
+
+    }
+
+    public void testDeleteUser() throws Exception {
+
+    }
+
+    public void testUpdateUser() throws Exception {
+
+    }
+
+    public void testGetUserById() throws Exception {
+
+    }
+
+    public void testGetUserByLogin() throws Exception {
+
+    }
+
+    public void testGetAllUsers() throws Exception {
+
+    }
+
+    public void testGetFriends() throws Exception {
+
+    }
+
+    public void testAddFriendship() throws Exception {
+
+    }
+}
