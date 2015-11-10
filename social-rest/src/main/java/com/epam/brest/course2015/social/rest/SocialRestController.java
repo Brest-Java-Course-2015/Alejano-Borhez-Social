cf7ddfa46d0d99af21b24f706187b6bf64e1046e
Alejano-Borhez <alexander.borohov17@gmail.com>
2015-11-10 03:25:27 +0300

    Rest module: Logger added

--- social-rest/src/main/java/com/epam/brest/course2015/social/rest/SocialRestController.java
+++ social-rest/src/main/java/com/epam/brest/course2015/social/rest/SocialRestController.java
@@ -3,12 +3,12 @@ package com.epam.brest.course2015.social.rest;
 import com.epam.brest.course2015.social.core.Friendship;
 import com.epam.brest.course2015.social.core.User;
 import com.epam.brest.course2015.social.service.SocialService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.jws.soap.SOAPBinding;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -17,39 +17,49 @@ import java.util.List;
 
 @RestController
 public class SocialRestController {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     @Autowired
     private SocialService socialService;
 
     @RequestMapping(value = "/users", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public @ResponseBody List<User> getAllUsers(/*@RequestParam(value = "crdatemin") Date createdDateMin,
-                                                @RequestParam(value = "crdatemax") Date createdDateMa*/) {return socialService.getAllUsers();}
+                                                @RequestParam(value = "crdatemax") Date createdDateMa*/) {
+        LOGGER.debug("rest: Getting all users");
+        return socialService.getAllUsers();}
 
     @RequestMapping(value = "/friendships", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
-    public @ResponseBody List<Friendship> getAllFriendships() {return socialService.getAllFriendships();}
+    public @ResponseBody List<Friendship> getAllFriendships() {
+        LOGGER.debug("rest: Getting all friendships");
+        return socialService.getAllFriendships();}
 
     @RequestMapping(value = "/user/friends", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public @ResponseBody List<User> getFriends(@RequestParam(value ="id") Integer id) {
+        LOGGER.debug("rest: Getting all friends of user {}", id);
         return socialService.getFriends(id);
     }
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public @ResponseBody User getUserById (@RequestParam(value = "id") Integer id) {
+        LOGGER.debug("rest: Getting user by id {}", id);
         return socialService.getUserById(id);
     }
 
     @RequestMapping(value = "/user/login", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public @ResponseBody User getUserByLogin (@RequestParam(value = "login") String login) {
+        LOGGER.debug("rest: Getting user by login {}", login);
         return socialService.getUserByLogin(login);
     }
 
     @RequestMapping(value = "/user", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public @ResponseBody Integer addUser (@RequestBody User user) {
+        LOGGER.debug("rest: Adding new user {}", user.getLogin());
         return socialService.addUser(user);
     }
 
@@ -57,12 +67,14 @@ public class SocialRestController {
     @ResponseStatus(value = HttpStatus.OK)
     public void updateUser(@RequestParam(value = "id") Integer id,
                            @RequestParam(value = "password") String password) {
+        LOGGER.debug("rest: Updating a user {}", id);
         socialService.updateUser(id, password);
     }
 
     @RequestMapping(value = "/user", method = RequestMethod.DELETE)
     @ResponseStatus(value = HttpStatus.OK)
     public void deleteUser(@RequestParam(value = "id") Integer id) {
+        LOGGER.debug("rest: Deleting a user {}", id);
         socialService.deleteUser(id);
     }
 
