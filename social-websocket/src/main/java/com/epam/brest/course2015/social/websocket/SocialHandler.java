6e4c016be1a52314104da1406e07859ac95cb265
Alejano-Borhez <alexander.borohov17@gmail.com>
2016-03-29 21:51:27 +0300

    WebSocket: simple @MessageMapping is done

--- social-websocket/src/main/java/com/epam/brest/course2015/social/websocket/SocialHandler.java
+++
@@ -1,7 +0,0 @@
-package com.epam.brest.course2015.social.websocket;
-
-/**
- * Created by alexander on 13.3.16.
- */
-public class SocialHandler {
-}
--- social-websocket/src/main/java/com/epam/brest/course2015/social/websocket/SocialWebSocket.java
+++ social-websocket/src/main/java/com/epam/brest/course2015/social/websocket/SocialWebSocket.java
@@ -1,18 +1,20 @@
 package com.epam.brest.course2015.social.websocket;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by alexander on 12.3.16.
  */
-
 @Controller
 public class SocialWebSocket {
 
+
     @MessageMapping("/hello")
-    public String sayHello() {
+    public String sayHello(String hello) {
+        System.out.println("You are using websockets " + hello + "!");
         return "Hello!";
+
     }
 
 }
